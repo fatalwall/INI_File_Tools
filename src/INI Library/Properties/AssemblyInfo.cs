23dd63078c148f5bf8dae8cd0af38fcacd310d05
Peter Varney <pvarney@vshed.us>
2019-05-09 22:34:58 -0400

    version increment v1.1

--- src/INI Library/Properties/AssemblyInfo.cs
+++ src/INI Library/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Peter Varney")]
 [assembly: AssemblyProduct("INI Library")]
-[assembly: AssemblyCopyright("Copyright © Peter Varney 2018")]
+[assembly: AssemblyCopyright("Copyright © 2018-2019 Peter Varney")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.*")]
+[assembly: AssemblyVersion("1.1.0.*")]
 
